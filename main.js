b47fe74fde0ac52a9119213e9c4544d7d7d95a9c
SebastianC <sebas99cano@gmail.com>
2021-05-26 12:16:16 -0500

    agregamos la funcion requestAnimationFrame, para estar actualizando el juego,cambiamos el nombre del main por controller,eliminamos la condicion que veficaba si un elemento era vacio o tenia el atributo kind antes del llegar al switch que grafica, debido a que este se estaba ejecutando demasiadas veces, agregamos la funcion clean en BoardView, agregamos la funcion Play en BoardView para controlar en esta funcion las funciones draw y clean de esta misma clase, creamos el objeto Ball por funciones, y lo instanciamos

--- main.js
+++ main.js
@@ -33,11 +33,25 @@
       this.y += this.speed;
     },
     up: function () {
-      this.y += this.speed;
+      this.y -= this.speed;
     },
     toString: function () {
       return "x: " + this.x + " y: " + this.y;
     },
+  };
+})();
+
+(function () {
+  self.Ball = function (x, y, radius, board) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.speed_y = 0;
+    this.speed_x = 3;
+    this.board = board;
+
+    board.ball = this;
+    this.kind = "circle";
   };
 })();
 
@@ -50,21 +64,33 @@
     this.ctx = canvas.getContext("2d");
   };
   self.BoardView.prototype = {
+    clean: function () {
+      this.ctx.clearRect(0, 0, this.board.width, this.board.height);
+    },
     draw: function () {
       for (var i = this.board.elements.length - 1; i >= 0; i--) {
         var el = this.board.elements[i];
         draw(this.ctx, el);
       }
     },
+    play: function () {
+      this.clean();
+      this.draw();
+    },
   };
   function draw(ctx, element) {
     //verificamos que el elemento no este vacio y que tenga la propiedad kind, para identificar que es un rectangulo en este caso
-    if (element !== null && element.hasOwnProperty("kind")) {
-      switch (element.kind) {
-        case "rectangle":
-          ctx.fillRect(element.x, element.y, element.width, element.height);
-          break;
-      }
+
+    switch (element.kind) {
+      case "rectangle":
+        ctx.fillRect(element.x, element.y, element.width, element.height);
+        break;
+      case "circle":
+        ctx.beginPath();
+        ctx.arc(element.x, element.y, element.radius, 0, 7);
+        ctx.fill();
+        ctx.closePath();
+        break;
     }
   }
 })();
@@ -74,9 +100,11 @@ var bar = new Bar(20, 100, 40, 100, board);
 var bar2 = new Bar(700, 100, 40, 100, board);
 var canvas = document.getElementById("canvas");
 var board_view = new BoardView(canvas, board);
+var ball = new Ball(350, 100, 10, board);
+
+window.requestAnimationFrame(controller);
 
 document.addEventListener("keydown", function (ev) {
-  console.log(ev.key);
   if (ev.key === "w") {
     ev.preventDefault();
     bar.up();
@@ -90,11 +118,9 @@ document.addEventListener("keydown", function (ev) {
     ev.preventDefault();
     bar2.down();
   }
-  console.log(""+bar);
-  console.log(""+bar2);
 });
-self.addEventListener("load", main);
 
-function main() {
-  board_view.draw();
+function controller() {
+  board_view.play();
+  window.requestAnimationFrame(controller);
 }
