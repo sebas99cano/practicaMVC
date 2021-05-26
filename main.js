e20c47e4871be588a4e7266bdc11d5840a2474d8
SebastianC <sebas99cano@gmail.com>
2021-05-26 13:27:41 -0500

    agregamos la fucnion check_collisions en el objeto BoardView, agregamos tambien la funcion hit con sus diferentes casos de colision, agregamos la funcion collision en el objeto Ball y a este mismo objeto le agregamos atributos necesarios para calcular su colision: bounce_angle, max_bounce_angle, speed. se agregaron mas comentarios

--- main.js
+++ main.js
@@ -53,6 +53,9 @@
     this.speed_x = 3;
     this.board = board;
     this.direction = 1;
+    this.bounce_angle = 0;
+    this.max_bounce_angle = Math.PI / 12; // angulo maximo en que la pelota puede salir disparada
+    this.speed = 3;
 
     board.ball = this;
     this.kind = "circle";
@@ -61,6 +64,25 @@
     move: function () {
       this.x += this.speed_x * this.direction;
       this.y += this.speed_y;
+    }, //witdh y heigth necesarios para calcular la funcion hit
+    get width() {
+      return this.radius * 2;
+    },
+    get height() {
+      return this.radius * 2;
+    },
+    //como debe salir la pelota en caso de colisionar
+    collision: function (bar) {
+      var relative_intersect_y = bar.y + bar.height / 2 - this.y;
+      var normalized_intersect_y = relative_intersect_y / (bar.height / 2);
+      this.bounce_angle = normalized_intersect_y * this.max_bounce_angle;
+
+      console.log(this.bounce_angle);
+
+      this.speed_y = this.speed * -Math.sin(this.bounce_angle);
+      this.speed_x = this.speed * Math.cos(this.bounce_angle);
+      if (this.x > this.board.width / 2) this.direction = -1;
+      else this.direction = 1;
     },
   };
 })();
@@ -83,14 +105,41 @@
         draw(this.ctx, el);
       }
     },
+    check_collisions: function () {
+      for (var i = this.board.bars.length - 1; i >= 0; i--) {
+        var bar = this.board.bars[i];
+        if (hit(bar, this.board.ball)) {
+          this.board.ball.collision(bar);
+        }
+      }
+    },
     play: function () {
       if (this.board.playing) {
         this.clean();
         this.draw();
-		this.board.ball.move();
+        this.check_collisions();
+        this.board.ball.move();
       }
     },
   };
+  function hit(a, b) {
+    //Revisa si a colisiona con bb
+    var hit = false;
+    //coliciones horizontales
+    if (b.x + b.width >= a.x && b.x < a.x + a.width) {
+      //colisiones verticales
+      if (b.y + b.height >= a.y && b.y < a.y + a.height) hit = true;
+    }
+    //colision de a con b
+    if (b.x <= a.x && b.x + b.width >= a.x + a.width) {
+      if (b.y <= a.y && b.y + b.height >= a.y + a.height) hit = true;
+    }
+    //colision de b con a
+    if (a.x <= b.x && a.x + a.width >= b.x + b.width) {
+      if (a.y <= b.y && a.y + a.height >= b.y + b.height) hit = true;
+    }
+    return hit;
+  }
   function draw(ctx, element) {
     //verificamos que el elemento no este vacio y que tenga la propiedad kind, para identificar que es un rectangulo en este caso
 
@@ -108,6 +157,7 @@
   }
 })();
 
+//inicializamos todas las variables globales
 var board = new Board(800, 400);
 var bar = new Bar(20, 100, 40, 100, board);
 var bar2 = new Bar(700, 100, 40, 100, board);
@@ -115,9 +165,12 @@ var canvas = document.getElementById("canvas");
 var board_view = new BoardView(canvas, board);
 var ball = new Ball(350, 100, 10, board);
 
+//dibujamos el juego por primera vez
 board_view.draw();
+//funcion que nos permite ejecutar el jeugo continuamente
 window.requestAnimationFrame(controller);
 
+//listener para mapear las teclas del juego
 document.addEventListener("keydown", function (ev) {
   if (ev.key === "w") {
     ev.preventDefault();
@@ -136,11 +189,7 @@ document.addEventListener("keydown", function (ev) {
     board.playing = !board.playing;
   }
 });
-
-setTimeout(function () {
-  ball.direction =1;
-}, 400);
-
+//funcion controller donde ejecutamos la funcion play
 function controller() {
   board_view.play();
   window.requestAnimationFrame(controller);
